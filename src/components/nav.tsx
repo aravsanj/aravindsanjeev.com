5a5ba117038f03a4b28ed4ec139d4f9e93c5832e
Aravind Sanjeev <saravind436@gmail.com>
2024-07-27 16:56:52 +0530

    feat: change to charcoal & gold theme

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -41,8 +41,10 @@ export function Navbar() {
                   key={path}
                   className={`group flex items-center py-3 cursor-pointer ${activeSection === name && "active"}`}
                 >
-                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-[#FEE715] group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                  <span
+                    className={`nav-text text-xs font-bold uppercase tracking-widest ${activeSection === name ? "text-[#FEE715]" : "text-slate-500"} group-hover:text-[#FEE715] group-focus-visible:text-[#FEE715]`}
+                  >
                     {name}
                   </span>
                 </a>
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Tag from "./ui/tag";
 
 interface ProjectProps {
   title: string;
@@ -14,11 +15,11 @@ export default function Project(props: ProjectProps) {
   return (
     <>
       <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-yellow-100/5 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
         <div className="z-10 sm:order-2 sm:col-span-6">
           <h3>
             <a
-              className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+              className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-[#FEE715]/80 focus-visible:text-[#FEE715]/80  group/link text-base"
               href={url}
               target="_blank"
               rel="noreferrer noopener"
@@ -50,10 +51,8 @@ export default function Project(props: ProjectProps) {
           <ul className="mt-2 flex flex-wrap" aria-label="Technologies used:">
             {tags.map((tag) => {
               return (
-                <li className="mr-1.5 mt-2">
-                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-                    {tag}
-                  </div>
+                <li key={tag} className="mr-1.5 mt-2">
+                  <Tag title={tag} />
                 </li>
               );
             })}
