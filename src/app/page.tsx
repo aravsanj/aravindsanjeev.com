25d20ede71ea8925ee470b4bd6c041a2422a2020
Aravind Sanjeev <saravind436@gmail.com>
2024-07-28 00:56:28 +0530

    fix: reduce margin and padding on phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     <div className="mx-auto min-h-screen max-w-[1280px] px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
         <Side />
-        <main className="pt-24 lg:w-1/2 lg:py-24 min-h-screen">
+        <main className="lg:pt-24 lg:w-1/2 lg:py-24 min-h-screen">
           <About />
           <Skills />
           <Projects />
