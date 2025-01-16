46bf3b28ed749113c3ee5c9bbed8454f3aedd180
Aravind Sanjeev <saravind436@gmail.com>
2025-01-16 23:13:42 +0530

    fix: responsiveness of blog listing

--- src/components/posts-by-tag.tsx
+++ src/components/posts-by-tag.tsx
@@ -16,21 +16,21 @@ export function BlogPostsByTag({ tag }: { tag: string }) {
         .map((post, index) => (
           <div
             key={post.slug}
-            className="flex items-center space-x-4 border-b  border-neutral-200 pb-4"
+            className="flex items-center space-x-4 border-b border-neutral-200 pb-4"
           >
             <span className="text-fuchsia-600 text-lg font-medium">
               {index + 1}.
             </span>
 
-            <div className="flex flex-col  md:flex-row md:items-center md:space-x-4">
+            <div className="flex flex-col md:flex-row md:items-center md:gap-12">
               <Link
                 href={`/blog/${post.slug}`}
-                className="text-gray-600 min-w-[400px]  font-semibold text-lg tracking-tight group-hover:underline hover:underline"
+                className="text-gray-600 sm:min-w-[400px]  font-semibold md:text-lg tracking-tight group-hover:underline hover:underline"
               >
                 {post.metadata.title}
               </Link>
 
-              <span className="text-gray-400 font-medium tabular-nums">
+              <span className="text-gray-400 min-w-[100px] text-sm md:text-base font-medium tabular-nums ">
                 {formatDate(post.metadata.publishedAt, false)}
               </span>
             </div>
