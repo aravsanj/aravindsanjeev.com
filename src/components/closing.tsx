f5cb58aa9e2b0a1bd126df7004dbb44bc01de971
Aravind Sanjeev <saravind436@gmail.com>
2025-01-18 18:32:00 +0530

    style: update closing

--- src/components/closing.tsx
+++ src/components/closing.tsx
@@ -4,26 +4,10 @@ export default function Closing() {
       <span className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
         Handcrafted with ❤️ drawing inspiration from{" "}
         <a
-          className="font-medium text-slate-400 hover:text-[#FEE715] focus-visible:text-[#FEE715]"
+          className="font-medium text-slate-900 hover:text-fuchsia-400 focus-visible:text-fuchsia-400"
           href="https://brittanychiang.com/"
         >
           Brittany Chiang
-        </a>
-        . The website is built on top of{" "}
-        <a
-          href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          className="font-medium text-slate-400 hover:text-[#FEE715] focus-visible:text-[#FEE715]"
-        >
-          Next.js Portfolio Starter Kit
-        </a>
-        . The source code is available on{" "}
-        <a
-          href="https://github.com/aravsanj/aravindsanjeev.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-medium text-slate-400 hover:text-[#FEE715] focus-visible:text-[#FEE715]"
-        >
-          GitHub.
         </a>
       </span>
     </section>
