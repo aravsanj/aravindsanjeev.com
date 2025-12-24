f47176d4b4bc430881ba2525daf302789ee45b02
Aravind Sanjeev <saravind436@gmail.com>
2025-12-24 23:56:18 +0530

    feat: Integrate Astro Partytown to offload Google Analytics scripts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aravindsanjeev.com',
+  site: "https://aravindsanjeev.com",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     sitemap(),
     mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
   markdown: {
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
     shikiConfig: {
-      theme: 'dracula',
-    }
-  }
+      theme: "dracula",
+    },
+  },
 });
